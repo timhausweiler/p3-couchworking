c496239ee8b8e98c05971617153fc60da47f61d3
Trung Diec <tdiec00@gmail.com>
2022-02-01 15:51:53 -0500

    removed second listen

--- src/server.js
+++ src/server.js
@@ -2,12 +2,19 @@ import express from "express";
 import logger from "morgan";
 import couches from "./routes/couches.js";
 import users from "./routes/users.js";
+import db from "./db/connection.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(logger("dev"));
 
 app.use("/user-api", users);
 app.use("/couch-api", couches);
+
+db.on("connected", () => {
+  app.listen(PORT, () => {
+    console.log(`Listening to PORT ${PORT}`);
+  });
+});
