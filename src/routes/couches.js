4e800e2b48cec075357fc98d679819bdee684129
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-02-02 11:48:51 -0500

    added couch routes for fetchall and fetchcouch

--- src/routes/couches.js
+++ src/routes/couches.js
@@ -2,4 +2,7 @@ import {Router} from "express";
 
 const router = Router();
 
+router.get("/couches", controllers.fetchAllCouches)
+router.get("/couch/:id", controllers.fetchCouch)
+
 export default router;
