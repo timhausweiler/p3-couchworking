b5f33844953945f1547c46205b6e805a0a07c403
Trung Diec <tdiec00@gmail.com>
2022-02-02 16:19:33 -0500

    async await removed

---
+++ src/utilities/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import logger from "morgan";
+import couches from "../routes/couches.js";
+import users from "../routes/users.js";
+import db from "../db/connection.js";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(logger("dev"));
+
+app.use("/user-api", users);
+app.use("/couch-api", couches);
+
+db.on("connected", () => {
+  app.listen(PORT, () => {
+    console.log(`Listening to PORT ${PORT}`);
+  });
+});
