74c438cbe0fa452154c53d2b731d9a475d226f03
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-02-02 11:44:38 -0500

    added fetchCouch and fetchAll Couches controllers

--- src/controllers/couch.js
+++ src/controllers/couch.js
@@ -1,2 +1,28 @@
 import Couch from "../models/couch.js";
 import errorHandler from "../utilities/error.js";
+
+export const fetchAllCouches = async (req, res) => {
+  try {
+    const couches = await Couch.find()
+    res.json(couches)
+    return res.json(errorHandler(false, "fetched all couches", couches))
+  } catch (error) {
+    console.log(error.message)
+    return res.json(errorHandler(true, "Error fetching all couches"))
+  }
+};
+
+export const fetchCouch = async (req,res) => {
+  try {
+    const { id } = req.params
+    const couch = await Couch.findById(id)
+    if (couch) {
+      return res.json(couch)
+    }
+    return res.json(errorHandler(true), "Couch not found")
+  } catch (error) {
+    console.log(error.message)
+    return res.json(errorHandler(true, "Error fetching couch"))
+  }
+};
+
