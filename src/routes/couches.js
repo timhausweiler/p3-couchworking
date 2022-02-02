ba60877a2392d1b623fbd206c808293137c81a70
Trung Diec <tdiec00@gmail.com>
2022-02-02 11:43:12 -0500

    edit couch route and controller

--- src/routes/couches.js
+++ src/routes/couches.js
@@ -1,5 +1,8 @@
 import {Router} from "express";
+import * as controllers from "../controllers/couch.js";
 
 const router = Router();
+
+router.put("/update", controllers.updateCouch);
 
 export default router;
