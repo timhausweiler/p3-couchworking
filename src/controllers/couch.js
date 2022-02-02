e9c142cad5511ec2830102883bc9e2e2090d3d60
Trung Diec <tdiec00@gmail.com>
2022-02-02 11:43:37 -0500

    edit route

--- src/controllers/couch.js
+++ src/controllers/couch.js
@@ -1,2 +1,24 @@
 import Couch from "../models/couch.js";
 import errorHandler from "../utilities/error.js";
+
+export const updateCouch = (req, res) => {
+  try {
+    Couch.findOneAndUpdate(
+      {_id: req.params.id},
+      req.body,
+      (error, updatedCouch) => {
+        if (updatedCouch) {
+          res.json(errorHandler(false, "Updated Couch", updatedCouch));
+        } else {
+          return res.json(
+            errorHandler(true, "Error updating couch", {
+              error: error.message,
+            })
+          );
+        }
+      }
+    );
+  } catch (error) {
+    return res.json(errorHandler(true, "Error updating couch"));
+  }
+};
