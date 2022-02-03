f6163728893526651e2cc8c5d9ab64d31b7df97b
Trung Diec <tdiec00@gmail.com>
2022-02-03 09:08:46 -0500

    all routes work now

--- src/utilities/restrict.js
+++ src/utilities/restrict.js
@@ -1,16 +1,16 @@
-import jwt from "jsonwebtoken"
-const SECRET = process.env.SECRET || "astronaut"
+import jwt from "jsonwebtoken";
+const SECRET = process.env.SECRET || "astronaut";
 
-const restrict = (req,res,next) => {
+const restrict = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1]
-    const data = jwt.verify(token, SECRET)
+    const token = req.headers.authorization.split(" ")[1];
+    const data = jwt.verify(token, SECRET);
     console.log(token);
-    next()
+    next();
   } catch (error) {
-    console.log(error)
-    return res.json(errorHandler(true, "unauthorized access"))
+    console.log(error);
+    return res.json(errorHandler(true, "unauthorized access"));
   }
-}
+};
 
 export default restrict;
