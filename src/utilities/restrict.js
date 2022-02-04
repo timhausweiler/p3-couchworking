194d00a571353995355b933517e2298f21560a76
Trung Diec <tdiec00@gmail.com>
2022-02-04 16:58:03 -0500

    auth works with local host

--- src/utilities/restrict.js
+++ src/utilities/restrict.js
@@ -1,15 +1,19 @@
 import jwt from "jsonwebtoken"
+import errorHandler from "./error.js"
 const SECRET = process.env.SECRET || "Astronaut"
 
 const restrict = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1]
-    const data = jwt.verify(token, SECRET)
+    const token = req.headers.authorization
     console.log(token)
+    // .split("")[1]
+    const data = jwt.verify(token, SECRET)
+
     next()
   } catch (error) {
+    // console.log(req.headers.authorization)
     console.log(error)
-    return res.json(errorHandler(true, "unauthorized access")).redirect("/sign-up")
+    return res.json(errorHandler(true, "unauthorized access"))
   }
 }
 
