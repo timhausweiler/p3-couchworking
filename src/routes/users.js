b7267da6e713650f8a8fa336125bde78f176f954
Trung Diec <tdiec00@gmail.com>
2022-02-04 14:43:22 -0500

    verivy route and function created

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
-import {Router} from "express";
-import * as controllers from "../controllers/user.js";
+import {Router} from "express"
+import * as controllers from "../controllers/user.js"
 
-const router = Router();
+const router = Router()
 
-router.post("/sign-up", controllers.signUp);
-router.post("/sign-in", controllers.signIn);
-router.get("/sign-out", controllers.signOut);
+router.post("/sign-up", controllers.signUp)
+router.post("/sign-in", controllers.signIn)
+router.get("/sign-out", controllers.signOut)
+router.get("/verify", controllers.verify)
 
-
-export default router;
+export default router
